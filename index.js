8809f7031eb5915d6743502b770fa1d5df7485db
Natacha <natacha.har@onja.org>
2020-11-24 16:31:44 +0300

    Controlled input

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+// 1. make the textarea a controlled input
+// 2. count the number of character of the input
+// 3. add the class if it's more than 280
+// 4. add the badge text to the textarea value when you click a badge
+
 const CharacterCounterInput = ({ text, defaults }) => {
 	const maxLength = 280;
 
+	const [mood, setMood] = useState('');
+
+	// function controlled(e) {
+	// 	const text = e.target.value;
+	// 	console.log(text.length)
+	// 	// if(text.length > 280) {
+	// 	// 	<textarea placeholder={text} onChange={controlled} />	
+	// 	// }
+	// 	setInputLenghtTextarea(inputLenghtTextarea);
+	// }
+
+
+
 	return (
-		<div className="counterInput">
+		<div className={`counterInput ${mood.length > maxLength && "tooLong"}`}>
 			<div>
 				{defaults.map(b => {
-					return <button key={b}>{b}</button>;
+					return <button key={b} onClick={() => {
+						setMood(b);
+					}}>{b}</button>;
 				})}
 			</div>
-			<textarea placeholder={text} />
-			<div>0/{maxLength}</div>
+			<textarea placeholder={text} onChange={(e) => setMood(e.target.value)} value={mood} />
+			<div>{mood.length}/{maxLength}</div>
 		</div>
 	);
 };
